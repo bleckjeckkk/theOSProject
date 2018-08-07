12d42deb38882c149896a76dcefa5da426c60090
Rommel Gallofin <rommelngallofin@su.edu.ph>
2018-08-07 21:54:49 +0800

    YEY

--- src/OSAlgos/FCFS.java
+++ src/OSAlgos/FCFS.java
@@ -97,43 +97,43 @@ class DisplayTimeline extends JPanel implements ActionListener{
         if((x > timeArr[0]*10 && timeArr[0]!=0)){
             g.setColor(Color.BLUE);
             g.fillRect(timeArr[0]*10,5,x,40);
-            g.drawString("Job 2:" + timeArr[1]*10, timeArr[0]*10,55);
+            g.drawString("Job 2:" + timeArr[1], timeArr[0]*10,55);
             i=1;
         }
         if(x > timeArr[1]*10 && timeArr[1]!=0){
             g.setColor(Color.RED);
             g.fillRect(timeArr[1]*10,5,x,40);
-            g.drawString("Job 3:" + timeArr[2]*10, timeArr[1]*10,55);
+            g.drawString("Job 3:" + timeArr[2], timeArr[1]*10,55);
             i=2;
         }
         if(x > timeArr[2]*10 && timeArr[2]!=0){
             g.setColor(Color.ORANGE);
             g.fillRect(timeArr[2]*10,5,x,40);
-            g.drawString("Job 4:" + timeArr[3]*10, timeArr[2]*10,55);
+            g.drawString("Job 4:" + timeArr[3], timeArr[2]*10,55);
             i=3;
         }
         if(x > timeArr[3]*10 && timeArr[3]!=0){
             g.setColor(Color.PINK);
             g.fillRect(timeArr[3]*10,5,x,40);
-            g.drawString("Job 5:" + timeArr[4]*10 , timeArr[3]*10,55);
+            g.drawString("Job 5:" + timeArr[4], timeArr[3]*10,55);
             i=4;
         }
         if(x >= timeArr[4]*10 && timeArr[4]!=0){
             g.setColor(Color.MAGENTA);
             g.fillRect(timeArr[4]*10,5,x,40);
-            g.drawString("Job 6:" + timeArr[5]*10, timeArr[4]*10,55);
+            g.drawString("Job 6:" + timeArr[5], timeArr[4]*10,55);
             i=5;
         }
         if(x > timeArr[5]*10 && timeArr[5]!=0){
             g.setColor(Color.CYAN);
             g.fillRect(timeArr[5]*10,5,x,40);
-            g.drawString("Job 7:" + timeArr[6]*10, timeArr[5]*10,55);
+            g.drawString("Job 7:" + timeArr[6], timeArr[5]*10,55);
             i=6;
         }
         if(x > timeArr[6]*10 && timeArr[6]!=0){
             g.setColor(Color.GRAY);
             g.fillRect(timeArr[6]*10,5,x,40);
-            g.drawString("Job 8:" + timeArr[7]*10, timeArr[6]*10,55);
+            g.drawString("Job 8:" + timeArr[7], timeArr[6]*10,55);
             i=7;
         }
         
